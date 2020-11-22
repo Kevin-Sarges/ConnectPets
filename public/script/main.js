36b9d533f00227941a7919e0af2b3c7d113e50eb
Kevin-Sarges <kevinsarges08@gmail.com>
2020-11-21 23:17:21 -0300

    Numero de whatsapp

--- public/script/main.js
+++ public/script/main.js
@@ -7,5 +7,5 @@ function adoption(){
 }
 
 function whatsapp(){
-    window.location.href = 'https://api.whatsapp.com/send?1=pt_BR&phone=55919738201&text=Desejo adotar o pet';
+    window.location.href = `https://api.whatsapp.com/send?1=pt_BR&phone={{pet.whatsapp}}&text=Desejo adotar o pet`;
 }
