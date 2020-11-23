5426ccb853f20c2494b51a29297c365483182e0a
Kevin-Sarges <kevinsarges08@gmail.com>
2020-11-23 12:04:11 -0300

    Data base

--- src/database/teste.js
+++ src/database/teste.js
@@ -4,15 +4,15 @@ const donatePets = require('./donatePet');
 Database.then( async(db) => {
     /*await donatePets(db, {
         photo: "https://source.unsplash.com/random".toString(),
-        name: "Kett Sarges",
+        name: "Kevin Sarges",
         race: "Pastor alemão",
         sex: "femia",
         age: "3 meses",
-        whatsapp: "999839137"
+        whatsapp: "999999999"
     });*/
 
     const selecterPets = await db.all('SELECT * FROM pets');
     console.log(selecterPets);
 
-    // console.log(await db.run('DELETE FROM pets WHERE id = "2"'));
+    // console.log(await db.run('DELETE FROM pets WHERE id = "4"'));
 });
