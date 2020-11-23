76fe24a4430cfc9776570121e27cca8b72e76cc5
Kevin-Sarges <kevinsarges08@gmail.com>
2020-11-23 12:03:47 -0300

    Scripts

--- public/script/main.js
+++ public/script/main.js
@@ -5,7 +5,3 @@ function donate(){
 function adoption(){
     window.location.href = '/adoption';
 }
-
-function whatsapp(){
-    window.location.href = `https://api.whatsapp.com/send?1=pt_BR&phone={{pet.whatsapp}}&text=Desejo adotar o pet`;
-}
