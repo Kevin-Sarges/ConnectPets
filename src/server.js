01ffd2edadc8bf0892fd11635cf56225fb6adac6
Kevin-Sarges <kevinsarges08@gmail.com>
2020-11-27 17:01:29 -0300

    Server

--- src/server.js
+++ src/server.js
@@ -6,12 +6,14 @@ const port = 5000;
 
 server.use(express.urlencoded({ extended: true}));
 server.use(express.static('public'));
+
 server.set('views',path.join(__dirname, 'views'));
 server.set('view engine', 'hbs');
 
 server.get('/', pages.index);
 server.get('/adoption', pages.adoption);
 server.get('/donate', pages.donate);
+
 server.post('/donatePet', pages.donatePet);
 
 server.listen(process.env.PORT || port, () => {
