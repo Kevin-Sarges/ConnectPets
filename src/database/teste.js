d48c5a0b499b0365c58bd2a1e97915fafd2439ea
Kevin-Sarges <kevinsarges08@gmail.com>
2020-11-27 17:10:18 -0300

    SQLite

--- src/database/teste.js
+++ src/database/teste.js
@@ -2,14 +2,14 @@ const Database = require('./db');
 const donatePets = require('./donatePet');
 
 Database.then( async(db) => {
-    /*await donatePets(db, {
+    await donatePets(db, {
         photo: "https://cdn.pixabay.com/photo/2019/07/12/21/47/kitten-4333662_960_720.jpg".toString(),
         name: "Kett Sarges",
         race: "Desconhecida",
         sex: "Macho",
         age: "1 meses",
         whatsapp: "88888888"
-    });*/
+    });
 
     const selecterPets = await db.all('SELECT * FROM pets');
     console.log(selecterPets);
