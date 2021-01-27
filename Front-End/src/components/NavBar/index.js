3dd29bec167eac21e2387db6e04c608060290264
Kevin-Sarges <kevinsarges08@gmail.com>
2021-01-27 20:05:33 -0300

    Connect Pets

---
+++ Front-End/src/components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Nav } from './styles';
+
+function NavBar() {
+  return (
+		<Nav></Nav>
+  );
+}
+
+export default NavBar;
---
+++ Front-End/src/components/NavBar/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Nav = styled.div`
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
