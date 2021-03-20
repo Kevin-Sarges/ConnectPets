b42e6471542ac6a2a78aeded70fe672b753e0b07
Kevin Sarges <kevinsarges08@gmail.com>
2021-03-20 18:51:33 -0300

    Axios post

--- Front-End/src/components/Form/index.js
+++ Front-End/src/components/Form/index.js
@@ -1,90 +1,107 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { Container, Fieldset, Legend, ImagesPet, Inputs, Footer } from './styles';
 import api from '../../services/api';
 
-class Form extends Component {
-  state = {
-    
+function Form() {
+  const [donate, setDonate] = useState({
+    photo: '',
+    age: '',
+    sex: '',
+    race: '',
+    name: '',
+    whatsapp: '', 
+  });
+
+
+  function handleInputChange(event) {
+    donate[event.target.name] = event.target.value;
+    setDonate(donate);
   }
 
-  handleChange = event => {
-    this.setState({
-      photo: event.target.value,
-      age: event.target.value,
-      sex: event.target.value,
-      race: event.target.value,
-      name: event.target.value,
-      whatsapp: event.target.value,
-    });
-  }
-
-  handleSubmit = event => {
+  function handleFormSubmit(event) {
     event.preventDefault();
-
-    const pet = {
-      photo: this.state.photo,
-      age: this.state.age,
-      sex: this.state.sex,
-      race: this.state.race,
-      name: this.state.name,
-      wahtsapp: this.state.whatsapp,
-    }
-
-    api.post('/donate', { pet })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
+    api.post('/donate', donate)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error)
       });
   }
 
-  render() {
-    return (
-      <Container>
-        <form onsubmit={this.handleSubmit} enctype="multipart/form-data">
-          <Fieldset>
-            <Legend>Peecha alguns dados</Legend>
-            
-            <ImagesPet>
-              <Inputs>
-                <label for="photo">Foto do pet</label>
-                <input id="photo" name="photo" type="file" onChange={this.handleChange} required/>
-              </Inputs>
-  
-              <Inputs>
-                <label for="age">Idade <span>(meses/anos)</span></label>
-                <input id="age" name="age" onChange={this.handleChange} required placeholder="Ex: 2 meses"/>
-              </Inputs>
-            </ImagesPet>
-  
+  return (
+    <Container>
+      <form onsubmit={handleFormSubmit} enctype="multipart/form-data">
+        <Fieldset>
+          <Legend>Peecha alguns dados</Legend>
+          
+          <ImagesPet>
             <Inputs>
-              <label for="sex">Sexo</label>
-              <input name="sex" onChange={this.handleChange} required/>
+              <label for="photo">Foto do pet</label>
+              <input 
+                id="photo" 
+                name="photo" 
+                type="file" 
+                onChange={handleInputChange} 
+                required
+              />
             </Inputs>
-  
+
             <Inputs>
-              <label for="race">Raça <span>(Se você não sabe coloque como "Desconhecida"!)</span></label>
-              <input name="race" onChange={this.handleChange} required/>
+              <label for="age">Idade <span>(meses/anos)</span></label>
+              <input 
+                id="age" 
+                name="age" 
+                placeholder="Ex: 2 meses" 
+                onChange={handleInputChange} 
+                required
+              />
             </Inputs>
-  
-            <Inputs>
-              <label for="name">Nome do doador <span>(Só os dois primeiros nomes!)</span></label>
-              <input name="name" onChange={this.handleChange} required/>
-            </Inputs>
-  
-            <Inputs>
-              <label for="whatsapp">Numero do whatsapp</label>
-              <input name="whatsapp" onChange={this.handleChange} required/>
-            </Inputs>
-  
-            <Footer>
-              <button type="submit">Cadastrar</button>
-            </Footer>
-          </Fieldset>
-        </form>
-      </Container>
-    );
-  }
+          </ImagesPet>
+
+          <Inputs>
+            <label for="sex">Sexo</label>
+            <input 
+              name="sex" 
+              require
+            />
+          </Inputs>
+
+          <Inputs>
+            <label for="race">Raça <span>(Se você não sabe coloque como "Desconhecida"!)</span></label>
+            <input 
+              name="race" 
+              onChange={handleInputChange} 
+              require
+            />
+          </Inputs>
+
+          <Inputs>
+            <label for="name">Nome do doador <span>(Só os dois primeiros nomes!)</span></label>
+            <input 
+              name="name" 
+              onChange={handleInputChange} 
+              require
+            />
+          </Inputs>
+
+          <Inputs>
+            <label for="whatsapp">Numero do whatsapp</label>
+            <input 
+              name="whatsapp" 
+              onChange={handleInputChange} 
+              require
+            />
+          </Inputs>
+
+          <Footer>
+            <button type="submit">Cadastrar</button>
+          </Footer>
+        </Fieldset>
+      </form>
+    </Container>
+  );
 }
 
 export default Form;
