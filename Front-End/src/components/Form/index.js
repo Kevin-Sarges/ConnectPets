52e4ee1fb15364f34a0abe1ade3a938a4b4ef82e
Kevin Sarges <kevinsarges08@gmail.com>
2021-03-21 00:18:15 -0300

    tantando add o metodo post do axios no form

--- Front-End/src/components/Form/index.js
+++ Front-End/src/components/Form/index.js
@@ -1,107 +1,124 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+//import { useNavigate } from "react-router-dom";
 
 import { Container, Fieldset, Legend, ImagesPet, Inputs, Footer } from './styles';
 import api from '../../services/api';
 
-function Form() {
-  const [donate, setDonate] = useState({
-    photo: '',
-    age: '',
-    sex: '',
-    race: '',
-    name: '',
-    whatsapp: '', 
-  });
-
-
-  function handleInputChange(event) {
-    donate[event.target.name] = event.target.value;
-    setDonate(donate);
+class Form extends Component {
+  state = {
+    dadosDonate: []
   }
 
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    api.post('/donate', donate)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error)
+  handleDonate = donates => {
+    const dadosDonate = donates.map(donate => ({
+      donate,
+      photo: donate.photo,
+      age: donate.age,
+      sex: donate.sex,
+      race: donate.race,
+      name: donate.name,
+      whatsapp: donate.whatsapp,
+    }));
+
+    this.setState({
+      dadosDonate: this.state.dadosDonate.concat(dadosDonate)
+    })
+  }
+
+  updateDonate = (id, data) => {
+    this.setState({ dadosDonate: this.state.dadosDonate.map(uploadDonate => {
+      return id === uploadDonate.id ? { ...uploadDonate, ...data} : uploadDonate;
+    }) })
+  }
+
+  onChange = (event) => {
+    const data = new FormData();
+
+    data.append('donatePet', event.donate)
+
+    api.post('/donate', data)
+    .then(response => {
+      this.updateDonate(this.updateDonate.id, {
+        id: response.data.id
       });
+    })
   }
 
-  return (
-    <Container>
-      <form onsubmit={handleFormSubmit} enctype="multipart/form-data">
-        <Fieldset>
-          <Legend>Peecha alguns dados</Legend>
-          
-          <ImagesPet>
+  render() {
+    return (
+      <Container>
+        <form onsubmit={this.handleDonate} enctype="multipart/form-data">
+          <Fieldset>
+            <Legend>Peecha alguns dados</Legend>
+            
+            <ImagesPet>
+              <Inputs>
+                <label for="photo">Foto do pet</label>
+                <input 
+                  id="photo" 
+                  name="photo" 
+                  type="file" 
+                  onChange={this.onChange} 
+                  required
+                />
+              </Inputs>
+  
+              <Inputs>
+                <label for="age">Idade <span>(meses/anos)</span></label>
+                <input 
+                  id="age" 
+                  name="age" 
+                  placeholder="Ex: 2 meses" 
+                  onChange={this.onChange} 
+                  required
+                />
+              </Inputs>
+            </ImagesPet>
+  
             <Inputs>
-              <label for="photo">Foto do pet</label>
+              <label for="sex">Sexo</label>
               <input 
-                id="photo" 
-                name="photo" 
-                type="file" 
-                onChange={handleInputChange} 
-                required
+                name="sex"
+                onChange={this.onChange}
+                require
               />
             </Inputs>
-
+  
             <Inputs>
-              <label for="age">Idade <span>(meses/anos)</span></label>
+              <label for="race">Raça <span>(Se você não sabe coloque como "Desconhecida"!)</span></label>
               <input 
-                id="age" 
-                name="age" 
-                placeholder="Ex: 2 meses" 
-                onChange={handleInputChange} 
-                required
+                name="race" 
+                onChange={this.onChange} 
+                require
               />
             </Inputs>
-          </ImagesPet>
-
-          <Inputs>
-            <label for="sex">Sexo</label>
-            <input 
-              name="sex" 
-              require
-            />
-          </Inputs>
-
-          <Inputs>
-            <label for="race">Raça <span>(Se você não sabe coloque como "Desconhecida"!)</span></label>
-            <input 
-              name="race" 
-              onChange={handleInputChange} 
-              require
-            />
-          </Inputs>
-
-          <Inputs>
-            <label for="name">Nome do doador <span>(Só os dois primeiros nomes!)</span></label>
-            <input 
-              name="name" 
-              onChange={handleInputChange} 
-              require
-            />
-          </Inputs>
-
-          <Inputs>
-            <label for="whatsapp">Numero do whatsapp</label>
-            <input 
-              name="whatsapp" 
-              onChange={handleInputChange} 
-              require
-            />
-          </Inputs>
-
-          <Footer>
-            <button type="submit">Cadastrar</button>
-          </Footer>
-        </Fieldset>
-      </form>
-    </Container>
-  );
+  
+            <Inputs>
+              <label for="name">Nome do doador <span>(Só os dois primeiros nomes!)</span></label>
+              <input 
+                name="name" 
+                onChange={this.onChange} 
+                require
+              />
+            </Inputs>
+  
+            <Inputs>
+              <label for="whatsapp">Numero do whatsapp</label>
+              <input 
+                name="whatsapp" 
+                onChange={this.onChange} 
+                require
+              />
+            </Inputs>
+  
+            <Footer>
+              <button type="submit">Cadastrar</button>
+            </Footer>
+          </Fieldset>
+        </form>
+      </Container>
+    );
+  }
 }
 
 export default Form;
