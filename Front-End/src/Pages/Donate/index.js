5a744078f073cd7305af3c84b7259c763612653f
Kevin Sarges <kevinsarges08@gmail.com>
2021-03-22 18:29:23 -0300

    Add e testando o DropZone

--- Front-End/src/Pages/Donate/index.js
+++ Front-End/src/Pages/Donate/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import Form from '../../components/Form';
+// import Form from '../../components/Form';
+import Dropzone from '../../components/DropZone';
 
 function Donate() {
   return (
     <>
-      <Form/>
+      <Dropzone/>
     </>
   );
 }
