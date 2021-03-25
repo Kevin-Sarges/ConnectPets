d7b9a1eadd2d46a3081f4a02716bd70d0a0328d1
Kevin Sarges <kevinsarges08@gmail.com>
2021-03-25 11:14:52 -0300

    Refatorando

--- Back-End/src/routes.js
+++ Back-End/src/routes.js
@@ -4,8 +4,6 @@ const controller = require('./controllers/controllersDonates');
 const multerConfig = require('./config/multerConfig');
 
 routes.get('/', controller.index);
-routes.get('/donate', controller.donate);
-routes.get('/information', controller.information);
 
 routes.post('/donate', multer(multerConfig).single('file'), controller.donatePet);
 
