b158ed018a77f80157fdd35d678c9545caf68152
Kevin Sarges <kevinsarges08@gmail.com>
2021-04-02 19:03:11 -0300

    Preloading

--- Front-End/src/components/Pets/index.js
+++ Front-End/src/components/Pets/index.js
@@ -1,46 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 
 import api from '../../services/api';
 
 import { Container, PreviewImage, Footer, Description, Button } from './styles';
 
-function Pets() {
-	const [pets, setPets] = useState([]);
+class Pets extends Component {
+	state = {
+		pets: [],
+	}
 
-	useEffect(() => {
-		api.get('/').then(response => {
-			setPets(response.data);
-		}).catch(error => {
+	async componentDidMount() {
+		try {
+			const response = await api.get('/');
+
+			this.setState({ pets: response.data });
+		} catch (error) {
 			console.log(error);
-		});
-	});
+		}
+	}
 
-	return (
-		<>
-			{pets.map(pet => (
-				<>
-					<Container key={ pet.id }>
-						<PreviewImage src={ pet.url } alt="Pets" />
+	render() {
 
-						<Footer>
-							<Description>
-								<p><b>Doador:</b> { pet.name }</p>
-								<p><b>Raça:</b> { pet.race }</p>
-								<p><b>Idade:</b> { pet.age }</p>
-								<p><b>Sexo:</b> { pet.sex }</p>
-							</Description>
-			
-							<footer>
-								<Button href={ `https://api.whatsapp.com/send?1=pt_BR&phone=${pet.whatsapp}&text=Desejo adotar pet` }>
-									Entre em contato
-								</Button>
-							</footer>
-						</Footer>
-					</Container>
-				</>
-			))}
-		</>
-	);
+		const { pets } = this.state;
+
+		return (
+			<>
+				{pets.map(pet => (
+					<>
+						<Container key={ pet.id }>
+							<PreviewImage src={ pet.url } alt="Pets" />
+
+							<Footer>
+								<Description>
+									<p><b>Doador:</b> { pet.name }</p>
+									<p><b>Raça:</b> { pet.race }</p>
+									<p><b>Idade:</b> { pet.age }</p>
+									<p><b>Sexo:</b> { pet.sex }</p>
+								</Description>
+				
+								<footer>
+									<Button href={ `https://api.whatsapp.com/send?1=pt_BR&phone=${pet.whatsapp}&text=Desejo adotar pet` }>
+										Entre em contato
+									</Button>
+								</footer>
+							</Footer>
+						</Container>
+					</>
+				))}
+			</>
+		);
+	}
 }
 
 export default Pets;
